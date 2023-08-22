145a70e03bf278c509d5640df6ad9b441cfa6e2b
Len Mahaffey <len.mahaffey@gmail.com>
2023-08-22 13:56:10 -0600

    cleanup

--- src/app/shared/d3/pie-chart/pie-chart.component.ts
+++ src/app/shared/d3/pie-chart/pie-chart.component.ts
@@ -14,24 +14,26 @@ export class PieChartComponent implements AfterViewInit{
   @Input() note = ''
   width = 250
   height = 250
-  margin = 0
-  radius = 0
   svg!: any
-  colors!: any
 
   @Input() data: PieChartData[] = []
 
-
   constructor() {
-    this.radius = Math.min(this.width, this.height) / 2 - this.margin;
-   }
+  }
+
   ngAfterViewInit(): void {
-          this.drawChart();
+    this.drawChart();
   }
 
   private drawChart(): void {
-    let tempId = "#" + this.id
-    this.svg = d3.select(tempId)
+    let colors = d3.scaleOrdinal()
+    .domain(this.data.map(d => d.Name))
+    .range(["#c7d3ec", "#a5b8db", "#879cc4", "#677795", "#5a6782"]);
+
+    let radius = Math.min(this.width, this.height) / 2;
+    // let radius = Math.min(this.width, this.height) / 2 - this.margin;
+
+    this.svg = d3.select("#" + this.id)
       .attr("width", this.width)
       .attr("height", this.height)
       .append("g")
@@ -39,10 +41,6 @@ export class PieChartComponent implements AfterViewInit{
         "transform",
         "translate(" + this.width / 2 + "," + this.height / 2 + ")"
       );
-
-    this.colors = d3.scaleOrdinal()
-      .domain(this.data.map(d => d.Name))
-      .range(["#c7d3ec", "#a5b8db", "#879cc4", "#677795", "#5a6782"]);
 
     // Compute the position of each group on the pie:
     const pie = d3.pie<PieChartData>().value((d: PieChartData) => Number(d.Value));
@@ -54,16 +52,16 @@ export class PieChartComponent implements AfterViewInit{
       .append('path')
       .attr('d', d3.arc()
         .innerRadius(0)
-        .outerRadius(this.radius)
+        .outerRadius(radius)
       )
-      .attr('fill', (d: PieChartData, i: any) => (this.colors(i)))
+      .attr('fill', (d: PieChartData, i: any) => (colors(i)))
       .attr("stroke", "#121926")
       .style("stroke-width", "1px");
 
     // Add labels
     const labelLocation = d3.arc()
       .innerRadius(25)
-      .outerRadius(this.radius);
+      .outerRadius(radius);
 
     this.svg.selectAll('pieces')
       .data(pie(this.data))
@@ -74,51 +72,5 @@ export class PieChartComponent implements AfterViewInit{
       .style("text-anchor", "middle")
       .style("font-size", 15);
   }
-  // private drawChart(): void {
-  //   let tempId = "svg"
-  //   this.svg = d3.select(tempId)
-  //     .attr("id", this.id)
-  //     .attr("width", this.width)
-  //     .attr("height", this.height)
-  //     .append("g")
-  //     .attr(
-  //       "transform",
-  //       "translate(" + this.width / 2 + "," + this.height / 2 + ")"
-  //     );
-
-  //   this.colors = d3.scaleOrdinal()
-  //     .domain(this.data.map(d => d.Name))
-  //     .range(["#c7d3ec", "#a5b8db", "#879cc4", "#677795", "#5a6782"]);
-
-  //   // Compute the position of each group on the pie:
-  //   const pie = d3.pie<PieChartData>().value((d: PieChartData) => Number(d.Value));
-
-  //   // Build the pie chart
-  //   this.svg.selectAll('pieces')
-  //     .data(pie(this.data))
-  //     .enter()
-  //     .append('path')
-  //     .attr('d', d3.arc()
-  //       .innerRadius(0)
-  //       .outerRadius(this.radius)
-  //     )
-  //     .attr('fill', (d: PieChartData, i: any) => (this.colors(i)))
-  //     .attr("stroke", "#121926")
-  //     .style("stroke-width", "1px");
-
-  //   // Add labels
-  //   const labelLocation = d3.arc()
-  //     .innerRadius(25)
-  //     .outerRadius(this.radius);
-
-  //   this.svg.selectAll('pieces')
-  //     .data(pie(this.data))
-  //     .enter()
-  //     .append('text')
-  //     .text((d: { data: PieChartData; }) => d.data.Name + " " + d.data.Value)
-  //     .attr("transform", (d: d3.DefaultArcObject) => "translate(" + labelLocation.centroid(d) + ")")
-  //     .style("text-anchor", "middle")
-  //     .style("font-size", 15);
-  // }
 }
 
